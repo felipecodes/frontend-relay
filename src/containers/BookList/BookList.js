dfe273690123e0423034d02ba115f104bbb26ffd
Felipe Oliveira <felipe.o.silva18@gmail.com>
2018-06-14 17:34:18 -0300

    add relay base structure

--- src/containers/BookList/BookList.js
+++ src/containers/BookList/BookList.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { graphql, QueryRenderer } from 'react-relay';
+import environment from '../../environment';
 
-const BookList = () => <div>BookList</div>;
-
-export default BookList;
+export default () => (
+  <QueryRenderer
+    environment={environment}
+    query={graphql`
+      query BookListQuery {
+        books {
+          edges {
+            cursor
+            node {
+              title
+            }
+          }
+        }
+      }
+    `}
+    variables={{}}
+    render={({ error, props }) => {
+      if (error) return <div>Error!</div>;
+      if (!props) return <div>Loading...</div>;
+      return <div>{JSON.stringify(props)}</div>;
+    }}
+  />
+);
---
+++ src/environment.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Environment, Network, RecordSource, Store } from 'relay-runtime';
+
+function fetchQuery(operation, variables) {
+  return fetch('http://127.0.0.1:4000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => response.json());
+}
+
+const environment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+export default environment;
